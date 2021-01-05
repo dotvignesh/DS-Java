6b0e3fa623c271bda2214b6a6f7f0ef3a23a2ff3
Vignesh Raja <vigneshvrs5@gmail.com>
2021-01-05 21:29:47 +0530

    added DLL base

---
+++ src/linkedList/DLL.java
@@ -0,0 +1,21 @@
+package linkedList;
+
+public class DLL {
+	
+	static class Node {
+		
+		int data;
+		Node prev;
+		Node next;
+		
+		Node(int d) {
+			data = d;
+			prev=null;
+			next=null;
+		}
+		
+	}
+	
+	
+
+}
