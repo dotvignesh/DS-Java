04e8b35bb6d1ecc38e13c04790f054a1a18bc2b0
Vignesh Raja <vigneshvrs5@gmail.com>
2021-01-06 21:43:24 +0530

    DLL

--- src/linkedList/DLL.java
+++ src/linkedList/DLL.java
@@ -33,8 +33,98 @@ public class DLL {
 		
 	}
 	
+//	Inserting element after a given node
+	public void insertAfter(Node prevNode, int data) {
+		
+		if (prevNode == null) {
+			System.out.println("The give node can't be null");
+			return;
+		}
+		
+		Node newNode = new Node(data);
+		
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+		newNode.prev = prevNode;
+		
+		if (newNode.next != null) {
+			newNode.next.prev = newNode;
+		}
+		
+	}
 	
+//	Inserting element before a given node
+	public void insertBefore(Node nextNode, int data) {
+		
+		if (nextNode == null) {
+			System.out.println("Given node can't be null");
+		}
+		
+		Node newNode = new Node(data);
+		
+		newNode.prev = nextNode.prev;
+		nextNode.prev = newNode;
+		newNode.next = nextNode;
+		
+		
+		if (nextNode.prev != null) {
+			nextNode.prev.next = newNode;
+		}
+		
+	}
 	
+//	Adding element at the end of the list
+	public void append(int data) {
+		
+		Node newNode = new Node(data);
+		Node lastNode = head;
+		
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		
+		while (lastNode.next != null) {
+			lastNode = lastNode.next;
+		}
+		
+		lastNode.next = newNode;
+		newNode.prev = lastNode;
+		
+	}
+
+	
+//	Print list
+	public void printList() {
+		
+//		Forward traversal 
+		
+		System.out.println("Forward traversal: ");
+		
+		Node currNode = head;
+		Node lastNode = null;
+		while (currNode != null) {
+			
+			System.out.print(currNode.data + " ");
+			lastNode = currNode;
+			currNode = currNode.next;
+			
+		}
+		
+//		Backward traversal
+		
+		System.out.println("\nBackward traversal: ");
+		
+		while (lastNode != null) {
+			
+			System.out.print(lastNode.data + " ");
+			lastNode = lastNode.prev;
+			
+		}
+		
+		System.out.println();
+		
+	}
 	
 
 }
--- src/linkedList/TestClass.java
+++ src/linkedList/TestClass.java
@@ -4,17 +4,29 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		
-		LinkedList list = new LinkedList();
+//		LinkedList list = new LinkedList();
+//		
+//		for (int i=1; i<=10; i++) {
+//			list.insert(i);
+//		}
+//		
+//		list.delete(9);
+//		list.delete(8);
+//		
+//		list.deleteAt(2);
+//		list.deleteAt(1);
+//		list.printList();
 		
-		for (int i=1; i<=10; i++) {
-			list.insert(i);
+		DLL list = new DLL();
+		
+		for (int i=1; i<=3; i++) {
+			list.append(i);
 		}
+
+		list.printList();
 		
-		list.delete(9);
-		list.delete(8);
-		
-		list.deleteAt(2);
-		list.deleteAt(1);
+		list.push(5);
+		list.insertAfter(list.head, 10);
 		list.printList();
 
 	}
